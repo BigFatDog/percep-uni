1835e6aa08ada39a6a9b64e606a012e67bbd72c8
Xing Yun <ink.xing.yun@gmail.com>
2018-03-21 20:19:55 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# DOCUMENTATION IS IN CONSTRUCTION
+
+
+# percep-uni
+
+Generate and refine perceptual uniform colors. This library can be used in browser as well as on node server.
+
+
+## Usage:
+
+1. Install
+
+```
+npm install percep-uni --save
+```
+
+2. ES6 Usage
+
+```
+import { generateColorPalette } from 'percep-uni';
+
+```
+
+## Development
+1. Clone repository
+2. Run commands
+```
+npm install         // install dependencies
+npm run dev         // view demos in web browser at localhost:3005
+npm run build       // build
+npm run test        // run tests only
+npm run test:cover  // run tests and view coverage report
+```
+
+## Credits
+This library was created based on [IWantHue by Medialab](http://tools.medialab.sciences-po.fr/iwanthue/). ALL core algorithms are developed by [IWantHue](http://tools.medialab.sciences-po.fr/iwanthue/) authors.
+percep-uni could be considered an api library of IWantHue application.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+
+
---
+++ rollup.config.dev.js
@@ -0,0 +1,48 @@
+import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import localResolve from 'rollup-plugin-local-resolve';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import json from 'rollup-plugin-json';
+
+export default {
+    input: 'src/index.js',
+    plugins: [
+        json({
+            exclude: [ 'node_modules' ],
+            preferConst: true,
+        }),
+        localResolve(),
+        babel(babelrc()),
+        resolve({
+            module: true,
+            jsnext: true,
+            main: true,
+            extensions: ['.js']
+        }),
+        commonjs(),
+        serve({
+            open: true,
+            verbose: true,
+            contentBase: ['demo', 'dist'],
+            historyApiFallback: false,
+            host: 'localhost',
+            port: 3004
+        }),
+        livereload({
+            watch: 'demo',
+            verbose: false
+        })
+    ],
+    external: [],
+    output: [
+        {
+            file: 'dist/percep-uni.standalone.js',
+            format: 'umd',
+            name: 'PercepUni',
+            sourcemap: true
+        }
+    ]
+};
